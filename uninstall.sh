c9b632b8e7e2a1de5259c6ea8bef8191f9f9c4be
Kzaark <kzaark1871@gmail.com>
2026-06-27 17:17:47 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Vérifie que le script est lancé en root
+# Vérification du que le script en root
 if [ "$(id -u)" -ne 0 ]; then
     echo "⚠️  Ce script doit être exécuté en tant que root (utilise 'sudo')."
     exit 1
 fi
 
-# Chemins (mis à jour pour "revolution")
+# Chemins
 INSTALL_DIR="/usr/local/share/revolution"
 BIN_DIR="/usr/local/bin"
 
-# Supprime le wrapper (pas un lien symbolique, mais un fichier)
+# Suppression wrappe
 rm -f "$BIN_DIR/revolution"
 
-# Supprime le dossier d'installation
+# Suppression dossier d'installation
 rm -rf "$INSTALL_DIR"
 
 # Message de confirmation
